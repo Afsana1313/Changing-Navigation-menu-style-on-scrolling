f64cd5cbfd595081ce7bfd27074b98f2d50d12f8
Afsana1313 <afsana2013nipa@gmail.com>
2019-08-29 09:58:36 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){ 
+  $('.door').click(function(){ 
+    $('.door').addClass('open'); 
+    $('.light').addClass('scale'); 
+    $('.door-frame').addClass('zoom'); 
+	setTimeout(function() {
+
+location.href = "http://primemychakras.com/steps-to-begin/";
+
+}, 5000);
+
+
+  });  
+});
